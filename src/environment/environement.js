72eb17bf6b77f4c972f99ea5f781606f129e079c
minh quang <minhquangkid@gmail.com>
2023-06-28 14:53:59 +0700

    change url BE

--- src/environment/environement.js
+++ src/environment/environement.js
@@ -1,4 +1,4 @@
 export class Environment {
     //static baseUrl = "http://localhost:5000";
-    static baseUrl = "https://booking-api.onrender.com";
+    static baseUrl = "https://booking-api-o8f2.onrender.com";
   }
